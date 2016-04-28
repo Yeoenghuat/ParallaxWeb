65c6b8425dd5b1c12a10a90a80e55226dce0c8fb
Eng Huat <August5th@live.com>
2016-04-28 18:57:03 +0800

    Lesson 1

---
+++ BlackBird/BlackBird/JS/myJavascript.js
@@ -0,0 +1,17 @@
+﻿var pContainerHeight = $('.bird-box').height();
+
+$(window).scroll(function (event) {
+    var scrollVal = $(window).scrollTop();
+    if (scrollVal <= pContainerHeight) {
+        $('.logo').css({
+            'transform': 'translate(0px, ' + scrollVal / 2 + '%)'
+        });
+        $('.back-bird').css({
+            'transform': 'translate(0px, ' + scrollVal / 4 + '%)'
+        });
+        $('.fore-bird').css({
+            'transform': 'translate(0px, -' + scrollVal / 40 + '%)'
+        });
+    }
+    console.log(scrollVal);
+})
