49b89870da05416cfde0dcefbee4759f14f389ad
Eng Huat <August5th@live.com>
2016-04-30 02:04:17 +0800

    lesson 3

--- BlackBird/BlackBird/JS/myJavascript.js
+++ BlackBird/BlackBird/JS/myJavascript.js
@@ -13,13 +13,18 @@ $(window).scroll(function (event) {
     });
 
     //console.log(scrollVal + ' ' + $('.clothes-pics').offset().top + ' ' + $(window).height() / 1.1);
-
-
     if (scrollVal > $('.clothes-pics').offset().top - ($(window).height() / 1.1)) { //set the class when scroll 10% of the screen
         $('.clothes-pics figure').each(function (i) {
             setTimeout(function () {
                 $('.clothes-pics figure').eq(i).addClass('is-showing'); // reset back the opacity in css
             }, 200 * (i + 1)); // timer value for each loop
+        });
+    };
+
+    if (scrollVal > $('.large-window').offset().top - ($(window).height() / 6)) {
+        $('.peeking-window p').css({
+            'opacity': '1',
+            'transform': 'translateX(0px)'
         });
     };
 })
