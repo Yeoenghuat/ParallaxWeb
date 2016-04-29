d3c99204153556227eb28d64f56d571567b2abb3
Eng Huat <August5th@live.com>
2016-04-29 21:17:27 +0800

    lesson 2

--- BlackBird/BlackBird/JS/myJavascript.js
+++ BlackBird/BlackBird/JS/myJavascript.js
@@ -2,16 +2,24 @@
 
 $(window).scroll(function (event) {
     var scrollVal = $(window).scrollTop();
-    if (scrollVal <= pContainerHeight) {
-        $('.logo').css({
-            'transform': 'translate(0px, ' + scrollVal / 2 + '%)'
+    $('.logo').css({
+        'transform': 'translate(0px, ' + scrollVal / 2 + '%)'
+    });
+    $('.back-bird').css({
+        'transform': 'translate(0px, ' + scrollVal / 4 + '%)'
+    });
+    $('.fore-bird').css({
+        'transform': 'translate(0px, -' + scrollVal / 40 + '%)'
+    });
+
+    //console.log(scrollVal + ' ' + $('.clothes-pics').offset().top + ' ' + $(window).height() / 1.1);
+
+
+    if (scrollVal > $('.clothes-pics').offset().top - ($(window).height() / 1.1)) { //set the class when scroll 10% of the screen
+        $('.clothes-pics figure').each(function (i) {
+            setTimeout(function () {
+                $('.clothes-pics figure').eq(i).addClass('is-showing'); // reset back the opacity in css
+            }, 200 * (i + 1)); // timer value for each loop
         });
-        $('.back-bird').css({
-            'transform': 'translate(0px, ' + scrollVal / 4 + '%)'
-        });
-        $('.fore-bird').css({
-            'transform': 'translate(0px, -' + scrollVal / 40 + '%)'
-        });
-    }
-    console.log(scrollVal);
+    };
 })
