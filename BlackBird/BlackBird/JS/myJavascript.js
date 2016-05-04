86ae718433c4ad5787f3ee772e39f5c98b1a0fb3
Eng Huat <August5th@live.com>
2016-05-05 05:03:36 +0800

    Lesson 6

--- BlackBird/BlackBird/JS/myJavascript.js
+++ BlackBird/BlackBird/JS/myJavascript.js
@@ -21,7 +21,7 @@ $(window).scroll(function (event) {
         });
     };
 
-    if (scrollVal > $('.large-window').offset().top - ($(window).height() / 6)) {
+    if (scrollVal > $('.large-window').offset().top - ($(window).height() / 7)) {
         $('.peeking-window p').css({
             'opacity': '1',
             'transform': 'translateX(0px)'
@@ -30,13 +30,29 @@ $(window).scroll(function (event) {
 
     if (scrollVal > $('.large-window').offset().top - $(window).height()) {
         $('.large-window').css({
-            'background-position': 'center ' + (scrollVal - $('.large-window').offset().top) + 'px'
+            'background-position': 'center ' + ((scrollVal - $('.large-window').offset().top) + 200) + 'px'
         });
 
         var opacity = (scrollVal - $('.large-window').offset().top + 400) / (scrollVal / 5);
 
         $('.window-tint').css({
             'opacity': opacity
+        });
+    }
+
+    if (scrollVal > $('.category-window').offset().top - $(window).height()) {
+        var offset = Math.min(0, scrollVal - $('.category-window').offset().top + $(window).height() - 600);
+
+        $('.category-item-1').css({
+            'transform': 'translate(' + offset + 'px, ' + Math.abs(offset * 0.2) + 'px)'
+        });
+        console.log(offset);
+        $('.category-item-2').css({
+            'transform': 'translate(0px, ' + Math.abs(offset) + 'px)'
+        });
+
+        $('.category-item-3').css({
+            'transform': 'translate(' + Math.abs(offset) + 'px, ' + Math.abs(offset * 0.2) + 'px)'
         });
     }
 })
